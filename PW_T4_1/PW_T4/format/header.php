e660f397207cc8e7b042f4dce287a96979feeae4
cassiano-sena <csdscr@gmail.com>
2023-05-21 19:12:17 -0300

    Add files via upload

---
+++ PW_T4_1/PW_T4/format/header.php
@@ -0,0 +1,19 @@
+<?php
+// include "connect.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto</title>
+    <link  rel="stylesheet" href="./assets/header.css"/>
+</head>
+<body>
+    <header class="header">
+        <span class="headerTitle"> Projeto</span> <!-- span é usado para fraseamento -->
+    </header>
+</body>
+</html>
