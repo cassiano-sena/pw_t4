55ab3c0fe7ee82c04de41fe4684fc36d92776b40
cassiano-sena <csdscr@gmail.com>
2023-05-21 19:13:33 -0300

    Add files via upload

---
+++ PW_T4/delete.php
@@ -0,0 +1,15 @@
+<?php
+include 'connect.php';
+if(isset($_GET['deleteid'])){ 
+    $id=$_GET['deleteid']; // pegando o dado deleteid e guardando dentro da variavel id
+
+    $sql="delete from `crud` where id=$id";
+    $result=mysqli_query($con,$sql);
+    if ($result) {
+        echo "Dados deletados com sucesso!";
+    } else{
+        die(mysqli_error($con));
+    }
+}
+
+?>
